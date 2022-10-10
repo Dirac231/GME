4812a466a90165e12030081d0f1a787d6432d52b
Dirac231 <74907503+Dirac231@users.noreply.github.com>
2022-10-10 17:17:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ homepath=/home/$(whoami)
 echo -e "Welcome to the GME installer!\n\n"
 
 read -p "Enter your GitHub username: " user
+read -p "Enter your GitHub e-mail: " email; git config --global user.email "$email"
 echo -e "\nHi $user! Go here: --> \"https://github.com/settings/tokens/new\" and create a token with \"repo\" scope\n"
 read -p "Please enter your token: " token; echo $token > $homepath/.git_tok
 
