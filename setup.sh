b34af9e3f5d5f29c3eb51b14d5cbdb18632a3cab
Dirac231 <74907503+Dirac231@users.noreply.github.com>
2022-10-10 17:34:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ echo -e "Welcome to the GME installer!\n\n"
 read -p "Enter your GitHub username: " user
 read -p "Enter your GitHub e-mail: " email; git config --global user.email "$email"
 echo -e "\nHi $user!" 
-echo -e "Go here: --> \"https://github.com/settings/tokens/new\" and create a token with \"repo\" and \"workflow\" scopes\n""
+echo -e "Go here: \"https://github.com/settings/tokens/new\" and create a token with \"repo\" and \"workflow\" scopes\n"
 read -p "Please enter your token: " token; echo $token > $homepath/.git_tok
 
 echo -e "\n#----GME FUNCTIONS----" >> $homepath/.bashrc; echo -e "git_user=\"$user\"" >> $homepath/.bashrc
 cat functions.sh >> $homepath/.bashrc; source $homepath/.bashrc
 
-echo -e "\nAll done! Execute "" Try to create a new repository with the command \"newrepo\""
+echo -e "\nAll done! Try to create a new repository with the command \"newrepo\""
