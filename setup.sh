ba4233b5f284113aebdbf7040f065295142bac69
Dirac231 <74907503+Dirac231@users.noreply.github.com>
2022-10-11 11:22:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,20 @@ echo -e "\nHi $user!"
 echo -e "Go here: \"https://github.com/settings/tokens/new\" and create a token with \"repo\" and \"workflow\" scopes\n"
 read -p "Please enter your token: " token; echo $token > $homepath/.git_tok
 
-echo -e "\n#----GME FUNCTIONS----" >> $homepath/.bashrc; echo -e "git_user=\"$user\"" >> $homepath/.bashrc
-cat functions.sh >> $homepath/.bashrc
-echo -e "\nAll done! Try to create a new repository with the command \"newrepo\""
-exec bash
+echo -e "\Select your version"
+echo "1) Bash"
+echo "2) ZSH"
+read sel
+
+if [ $sel = 1 ]
+then
+  echo -e "\n#----GME FUNCTIONS----" >> $homepath/.bashrc; echo -e "git_user=\"$user\"" >> $homepath/.bashrc
+  cat bash_functions.sh >> $homepath/.bashrc
+  echo -e "\nAll done! Try to create a new repository with the command \"newrepo\""
+  exec bash
+else
+  echo -e "\n#----GME FUNCTIONS----" >> $homepath/.zshrc; echo -e "git_user=\"$user\"" >> $homepath/.zshrc
+  cat zsh_functions.sh >> $homepath/.zshrc
+  echo -e "\nAll done! Try to create a new repository with the command \"newrepo\""
+  exec zsh
+fi
